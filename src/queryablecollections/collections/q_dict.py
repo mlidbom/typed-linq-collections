fcf1494de507284cd4550f3bbdf7d55036bcf101
Magnus Lidbom <magnus.lidbom@gmail.com>
2025-09-20 19:49:09 +0200

    starting in on QDict

---
+++ src/queryablecollections/collections/q_dict.py
@@ -0,0 +1,19 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING, Iterable, override
+
+from queryablecollections._private_implementation_details.q_zero_overhead_collection_contructors import ZeroImportOverheadConstructors as C
+from queryablecollections.q_iterable import QIterable
+
+if TYPE_CHECKING:
+    pass
+
+class QDict[TKey, TItem](dict[TKey, TItem], QIterable[TKey]):
+    __slots__: tuple[str, ...] = ()
+    def __init__(self, elements: Iterable[tuple[TKey, TItem]]) -> None:
+        super().__init__(elements)
+
+    def qitems(self) -> QIterable[tuple[TKey, TItem]]: return C.iterable(super().items())
+
+    @override
+    def _optimized_length(self) -> int: return len(self)
