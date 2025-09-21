a9c556f0ed044ced62e6951d703b5ac514398c21
Magnus Lidbom <magnus.lidbom@gmail.com>
2025-09-22 01:51:48 +0200

    refactoring

--- queryablecollections_tests/test_any.py
+++ queryablecollections_tests/test_any.py
@@ -3,5 +3,12 @@ from __future__ import annotations
 from test_iterable_common import value_test
 
 
-def test_any_returns_true_if_there_are_elements() -> None: value_test([1], lambda x: x.any(), True)
-def test_any_returns_false_if_there_are_no_elements() -> None: value_test([], lambda x: x.any(), False)
+def test_any_returns_true_if_there_are_elements() -> None:
+    value_test([1],
+               lambda x: x.any(),
+               True)
+
+def test_any_returns_false_if_there_are_no_elements() -> None:
+    value_test([],
+               lambda x: x.any(),
+               False)
--- queryablecollections_tests/test_indexer.py
+++ queryablecollections_tests/test_indexer.py
@@ -3,6 +3,17 @@ from __future__ import annotations
 from test_iterable_common import value_test
 
 
-def test_indexer_returns_first_value() -> None: value_test([1, 2, 3], lambda x: x.to_list()[0], 1)
-def test_indexer_returns_middle_value() -> None: value_test([1, 2, 3], lambda x: x.to_list()[1], 2)
-def test_indexer_returns_last_value() -> None: value_test([1, 2, 3], lambda x: x.to_list()[2], 3)
+def test_indexer_returns_first_value() -> None:
+    value_test([1, 2, 3],
+               lambda x: x.to_list()[0],
+               1)
+
+def test_indexer_returns_middle_value() -> None:
+    value_test([1, 2, 3],
+               lambda x: x.to_list()[1],
+               2)
+
+def test_indexer_returns_last_value() -> None:
+    value_test([1, 2, 3],
+               lambda x: x.to_list()[2],
+               3)
