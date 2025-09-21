86e7fdbd6f386dab20e11fd359a04c11dde168fd
Magnus Lidbom <magnus.lidbom@gmail.com>
2025-09-22 01:59:34 +0200

    refactoring

--- queryablecollections_tests/test_concat.py
+++ queryablecollections_tests/test_concat.py
@@ -3,4 +3,7 @@ from __future__ import annotations
 from test_iterable_common import value_test
 
 
-def test_concat_appends_second_sequence_to_first() -> None: value_test([1, 2], lambda x: x.concat([3, 4]).to_list(), [1, 2, 3, 4])
+def test_concat_appends_second_sequence_to_first() -> None:
+    value_test([1, 2],
+               lambda x: x.concat([3, 4]).to_list(),
+               [1, 2, 3, 4])
