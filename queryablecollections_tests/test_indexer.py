e76e7d29259d11d46d2860d0cf24089d669e6ec1
Magnus Lidbom <magnus.lidbom@gmail.com>
2025-09-22 01:23:45 +0200

    refactoring

---
+++ queryablecollections_tests/test_indexer.py
@@ -0,0 +1,8 @@
+from __future__ import annotations
+
+from test_q_iterable_common import value_test
+
+
+def test_indexer_returns_first_value() -> None: value_test([1, 2, 3], lambda x: x.to_list()[0], 1)
+def test_indexer_returns_middle_value() -> None: value_test([1, 2, 3], lambda x: x.to_list()[1], 2)
+def test_indexer_returns_last_value() -> None: value_test([1, 2, 3], lambda x: x.to_list()[2], 3)
