964a3a50bd8786ed8f5ba96a2f0cc714f6507de7
Magnus Lidbom <magnus.lidbom@gmail.com>
2025-09-22 03:18:23 +0200

    refactoring

--- queryablecollections_tests/q_iterable/test_distinct.py
+++ queryablecollections_tests/q_iterable/test_distinct.py
@@ -9,14 +9,12 @@ def test_distinct_removes_duplicates_while_retaining_order() -> None:
                [1, 2, 3])
 
 def test_distinct_by_removes_duplicates_by_selected_key_while_retaining_order_keeping_the_first_matched_value() -> None:
-    value_test([
-            ("a", 1),
-            ("a", 2),
-            ("b", 3),
-            ("a", 4),
-            ("b", 5)],
-            lambda x: x.distinct_by(lambda y: y[0]).to_list(),
-            [
-                    ("a", 1),
-                    ("b", 3)],
-            skip_sets=True)
+    value_test([("a", 1),
+                ("a", 2),
+                ("b", 3),
+                ("a", 4),
+                ("b", 5)],
+               lambda x: x.distinct_by(lambda y: y[0]).to_list(),
+               [("a", 1),
+                ("b", 3)],
+               skip_sets=True)
--- queryablecollections_tests/q_iterable/test_first.py
+++ queryablecollections_tests/q_iterable/test_first.py
@@ -3,10 +3,30 @@ from __future__ import annotations
 from common_helpers import throws_test, value_test
 
 
-def test_first_returns_first_value() -> None: value_test([1, 2, 3], lambda x: x.first(), 1)
-def test_first_throws_if_no_values() -> None: throws_test([], lambda x: x.first())
-def test_first_returns_single_none_value() -> None: value_test([None], lambda x: x.first(), None)
+def test_first_returns_first_value() -> None:
+    value_test([1, 2, 3],
+               lambda x: x.first(),
+               1)
 
-def test_first_or_none_returns_first_value() -> None: value_test([1, 2, 3], lambda x: x.first_or_none(), 1)
-def test_first_or_none_return_none_if_no_values() -> None: value_test([], lambda x: x.first_or_none(), None)
-def test_first_or_none_returns_single_none_value() -> None: value_test([None], lambda x: x.first_or_none(), None)
+def test_first_returns_single_none_value() -> None:
+    value_test([None],
+               lambda x: x.first(),
+               None)
+
+def test_first_or_none_returns_first_value() -> None:
+    value_test([1, 2, 3],
+               lambda x: x.first_or_none(),
+               1)
+
+def test_first_or_none_return_none_if_no_values() -> None:
+    value_test([],
+               lambda x: x.first_or_none(),
+               None)
+
+def test_first_or_none_returns_single_none_value() -> None:
+    value_test([None],
+               lambda x: x.first_or_none(),
+               None)
+
+def test_first_throws_if_no_values() -> None:
+    throws_test([], lambda x: x.first())
