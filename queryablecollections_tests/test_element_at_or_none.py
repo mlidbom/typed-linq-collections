1485fcfee356f6ab9d5bc68f3a823d6089b26757
Magnus Lidbom <magnus.lidbom@gmail.com>
2025-09-22 02:01:00 +0200

    refactor

--- queryablecollections_tests/test_element_at_or_none.py
+++ queryablecollections_tests/test_element_at_or_none.py
@@ -3,27 +3,22 @@ from __future__ import annotations
 from test_iterable_common import value_test
 
 
-def test_or_none_returns_first_element() -> None:
+def test_0_test_returns_element_1() -> None:
     value_test([1, 2, 3],
                lambda x: x.element_at_or_none(0),
                1)
 
-def test_or_none_return_middle_element() -> None:
+def test_1_return_element_2() -> None:
     value_test([1, 2, 3],
                lambda x: x.element_at_or_none(1),
                2)
 
-def test_or_none_returns_last_element() -> None:
+def test_2_returns_element_3() -> None:
     value_test([1, 2, 3],
                lambda x: x.element_at_or_none(2),
                3)
 
-def test_or_none_returns_none_if_index_is_out_of_range() -> None:
-    value_test([1, 2, 3],
-               lambda x: x.element_at_or_none(3),
-               None)
-
-def test_or_none_after_to_list_returns_none_if_index_is_out_of_range() -> None:
+def test_returns_none_if_index_is_out_of_range() -> None:
     value_test([1, 2, 3],
                lambda x: x.element_at_or_none(3),
                None)
