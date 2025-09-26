50699491b1255987e1644ba4cea9301ef852978d
Magnus Lidbom <magnus.lidbom@gmail.com>
2025-09-26 23:12:37 +0200

    Mess around with the .toml preparing for publishing

--- repomix-wrapper.bat
+++ repomix-wrapper.bat
@@ -1,3 +1,3 @@
 @echo off
 call "%~dp0venv\Scripts\activate.bat"
-npx repomix@latest --output ./repomix/repomix-typed-linq.xml --include "**/*.py,**/*.pyi,**/*.toml,**/*.md,licence.txt" --ignore "_lib,user_files" ..\typed-linq-collections
+npx repomix@latest --output ./repomix/repomix-typed-linq.xml --ignore "_lib,user_files" ..\typed-linq-collections
