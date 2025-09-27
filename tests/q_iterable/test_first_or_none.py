ebd7adb5cdca3483c1e3402917e7cf0e7b58c150
Magnus Lidbom <magnus.lidbom@gmail.com>
2025-09-27 09:46:31 +0200

    add tests for lambda version of first_or_none

--- tests/q_iterable/test_first_or_none.py
+++ tests/q_iterable/test_first_or_none.py
@@ -17,3 +17,18 @@ def test_returns_single_none_value() -> None:
     value_test_including_unordered_collections([None],
                                                lambda x: x.first_or_none(),
                                                None)
+
+def test_with_predicate_returns_first_matching_value() -> None:
+    value_test_including_unordered_collections([1, 2, 3],
+                                               lambda x: x.first_or_none(lambda v: v > 1),
+                                               2)
+
+def test_with_predicate_returns_none_if_no_match() -> None:
+    value_test_including_unordered_collections([1, 2, 3],
+                                               lambda x: x.first_or_none(lambda v: v > 10),
+                                               None)
+
+def test_with_predicate_returns_none_if_empty() -> None:
+    value_test_including_unordered_collections([],
+                                               lambda x: x.first_or_none(lambda v: v is not None),
+                                               None)
