f477b9fc487f76c79941875836ad4eaad172c814
Magnus Lidbom <magnus.lidbom@gmail.com>
2025-09-27 02:51:29 +0200

    improve on the docs

--- tests/test_quickstart.py
+++ tests/test_quickstart.py
@@ -2,8 +2,9 @@ from typed_linq_collections.collections.q_list import QList
 from typed_linq_collections.q_iterable import query
 
 
+fruits = ("apple", "apricot", "mango", "melon", "peach", "pineapple")
 def test_querying_built_in_collections() -> None:
-    fruits_by_first_character = (query(["apple", "apricot",  "mango", "melon", "peach", "pineapple"])
+    fruits_by_first_character = (query(fruits)
                                  .group_by(lambda fruit: fruit[0])
                                  .where(lambda group: group.key in {"a", "p"})
                                  .to_list())
@@ -11,9 +12,17 @@ def test_querying_built_in_collections() -> None:
     assert fruits_by_first_character == [['apple', 'apricot'], ['peach', 'pineapple']]
 
 def test_querying_with_queryable_collections() -> None:
-    fruits_by_first_character = (QList(("apple", "apricot",  "mango", "melon", "peach", "pineapple"))
+    fruits_by_first_character = (QList(fruits)
                                  .group_by(lambda fruit: fruit[0])
                                  .where(lambda group: group.key in {"a", "p"})
                                  .to_list())
 
     assert fruits_by_first_character == [['apple', 'apricot'], ['peach', 'pineapple']]
+
+def test_numeric_operations() -> None:
+    total_lenght_of_fruits = (query(fruits)
+                              .select(len)
+                              .as_ints() # get's you a QIntIterable with numeric operations support. typed so that it is only available on a QIterable[int]
+                              .sum())
+
+    assert total_lenght_of_fruits == 36
